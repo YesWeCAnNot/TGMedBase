f75c661eb0656e943e47b4cfae1ef1bf4cb1e89e
YesWeCAnNot <94593709+YesWeCAnNot@users.noreply.github.com>
2024-09-08 17:23:24 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,12 @@
+# Users
+ID_ADMINISTRATOR: set[int] = {1, 2}  
+ID_USER = (1, )
+
+# TABLE_NAME = (1045974556,)
+TABLE_NAME = {
+    1: "USER_NAME",
+    2: "USER_NAME"
+}
+
+# Token Bot
+API_TOKEN = "API_TOKEN"
